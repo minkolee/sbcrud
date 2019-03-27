82cac00a892aaea37b4a331b49819e94125e1211
minkolee <minkolee@gmail.com>
2019-03-27 16:55:39 +0800

    开始写Spring Boot的CRUD

---
+++ src/main/java/cc/conyli/sbcrud/SbcrudApplication.java
@@ -0,0 +1,13 @@
+package cc.conyli.sbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/conyli/sbcrud/SbcrudApplicationTests.java
@@ -0,0 +1,16 @@
+package cc.conyli.sbcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbcrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
