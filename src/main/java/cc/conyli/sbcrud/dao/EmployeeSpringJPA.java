959342f74f91b7971c8329ec583cb2b8e0322270
minkolee <minkolee@gmail.com>
2019-03-28 15:47:13 +0800

    完成Thymeleaf增删改查

--- src/main/java/cc/conyli/sbcrud/dao/EmployeeSpringJPA.java
+++ src/main/java/cc/conyli/sbcrud/dao/EmployeeSpringJPA.java
@@ -2,9 +2,7 @@ package cc.conyli.sbcrud.dao;
 
 import cc.conyli.sbcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
 public interface EmployeeSpringJPA extends JpaRepository<Employee, Integer> {
 
 }
